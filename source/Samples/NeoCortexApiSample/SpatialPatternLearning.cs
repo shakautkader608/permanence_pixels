ad4a0323696bb2576f24b9da1fec8c2fe4f1b618
Md Shakaut kader <shakautkader608@gmail.com>
2025-01-13 00:49:26 +0100

    Experiment SpartialPattenr witn 800 Cycle

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
             }
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 700;
+            int maxSPLearningCycles = 800;
 
             int numStableCycles = 0;
 
