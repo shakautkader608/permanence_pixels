fa94b5f6d703de25c6478c0e77feddf389d1a4a2
Md Shakaut kader <shakautkader608@gmail.com>
2025-01-13 00:09:30 +0100

    Exp spatialPattern

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
             }
 
             // Learning process will take 1000 iterations (cycles)
-            int maxSPLearningCycles = 1000;
+            int maxSPLearningCycles = 100;
 
             int numStableCycles = 0;
 
