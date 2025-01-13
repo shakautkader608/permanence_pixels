7e162cd58e65d02f6d6d1ca6a551aa6e636534f2
Trtamim <trtamim041@gmail.com>
2025-01-13 11:40:59 +0100

    maxboost changing for spatial learning pattern

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -24,8 +24,8 @@ namespace NeoCortexApiSample
             experiment.Run();
 
             // Starts experiment For the Image Inputs how to learn spatial patterns.
-            //ImageBinarizerSpatialPattern experiment = new ImageBinarizerSpatialPattern();
-            //experiment.Run();
+            ImageBinarizerSpatialPattern experiment = new ImageBinarizerSpatialPattern();
+            experiment.Run();
 
 
             //
