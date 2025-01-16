8ad2357fa10847a15d5ae8fa9b2818cb6c76b86e
Jakia <jakiasultanaj@gmail.com>
2025-01-16 21:32:38 +0100

    checking outputs with diffrent values

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -28,7 +28,7 @@ namespace NeoCortexApiSample
             double maxBoost = 5.0;
 
             // We will use 200 bits to represent an input vector (pattern).
-            int inputBits = 150;
+            int inputBits = 200;
 
             // We will build a slice of the cortex with the given number of mini-columns
             int numColumns = 1024;
