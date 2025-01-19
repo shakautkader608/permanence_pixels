29b2b4f41b79394c27ded14e3c21a823ee4a5ad5
Jakia <jakiasultanaj@gmail.com>
2025-01-19 23:52:24 +0100

    experimenting with double max values

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -53,7 +53,7 @@ namespace NeoCortexApiSample
                 StimulusThreshold= 15, // Added a space after `StimulusThreshold`
             };
 
-            double max = 150;
+            double max = 100;
 
             //
             // This dictionary defines a set of typical encoder parameters.
