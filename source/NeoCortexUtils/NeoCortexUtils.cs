26d2a9e5c356beaa437769f61d6213e6fe9e6c8a
Md Shakaut kader <shakautkader608@gmail.com>
2025-02-22 20:56:06 +0100

    Generate and draw heatmap based on permanence values

--- source/NeoCortexUtils/NeoCortexUtils.cs
+++ source/NeoCortexUtils/NeoCortexUtils.cs
@@ -1186,7 +1186,6 @@ namespace NeoCortex
                     int red = Math.Min(255, (int)(255 * (permanence / maxPermanence)));
                     int blue = Math.Min(255, (int)(255 * (1 - permanence / maxPermanence)));
                     Color coloredPixelColor = Color.FromArgb(red, 0, blue);
-
                     int x = i % gridSize;
                     int y = i / gridSize;
                     int scaleX = gridWidth;
@@ -1196,11 +1195,9 @@ namespace NeoCortex
                         coloredGraphics.FillRectangle(brush, x * scaleX, y * scaleY +
                         titlePadding, scaleX, scaleY);
                     }
-
                 }
-
-
             }
+
 
 
 
