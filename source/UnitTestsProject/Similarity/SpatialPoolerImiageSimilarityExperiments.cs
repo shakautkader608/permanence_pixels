bd9478b15c10864c5dcf1b619320096d7a04c6ce
Trtamim <trtamim041@gmail.com>
2025-03-19 13:24:32 +0100

    Add initial test class structure for image similarity experiments

--- source/UnitTestsProject/Similarity/SpatialPoolerImiageSimilarityExperiments.cs
+++ source/UnitTestsProject/Similarity/SpatialPoolerImiageSimilarityExperiments.cs
@@ -13,3 +13,13 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+namespace UnitTestsProject
+{
+    [TestClass]
+    [TestCategory("Experiment")]
+    public class SpatialPoolerImageSimilarityExperiments
+    {
+        private const int OutImgSize = 1024;
+        private const string TestOutputFolder = "Output-ImageSimilarityExperiment";
+    }
+}
