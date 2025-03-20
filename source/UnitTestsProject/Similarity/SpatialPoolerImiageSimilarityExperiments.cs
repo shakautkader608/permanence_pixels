0f39b8da5c7e108e51e04ffb38d5c060e507b1d1
Ipti11 <zariaipti1@gmail.com>
2025-03-20 03:26:45 +0100

    Calculating similarity between Array

--- source/UnitTestsProject/Similarity/SpatialPoolerImiageSimilarityExperiments.cs
+++ source/UnitTestsProject/Similarity/SpatialPoolerImiageSimilarityExperiments.cs
@@ -173,6 +173,7 @@ namespace UnitTestsProject
                         {
                             int[] sdr1 = sdrs[key1];
                             int[] sdr2 = sdrs[key2];
+                            double outputSimilarity = MathHelpers.CalcArraySimilarity(sdr1, sdr2);
 
                         }
                     }
